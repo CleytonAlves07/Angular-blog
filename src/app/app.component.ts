f4e482e289093dd9b77f47c9484ebfa08bcab57c
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-03-09 10:55:53 -0300

    feat: :sparkles: Incluindo componentes do blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainCardComponent } from "./components/main-card/main-card.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SideCardsComponent } from "./components/side-cards/side-cards.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MainCardComponent,
+    NavbarComponent,
+    SideCardsComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/side-cards/side-cards.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-cards',
+  imports: [],
+  templateUrl: './side-cards.component.html',
+  styleUrl: './side-cards.component.css'
+})
+export class SideCardsComponent {
+
+}
