a03b64a836118227b9b815285b67e45bdfc2e2c0
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-03-09 17:18:46 -0300

    feat: :sparkles: Criando uma estrutura new card para servir de molde para os side cards

---
+++ src/app/components/news-card/news-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-news-card',
+  standalone: true,
+  templateUrl: './news-card.component.html',
+  styleUrls: ['./news-card.component.css']
+})
+export class NewsCardComponent {
+  @Input() news: any;
+}
